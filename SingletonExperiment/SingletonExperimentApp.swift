ed5ec080c7dd400b4bd041d18ce162a468b0a729
Aman Aggarwal <akaggarw@ucsd.edu>
2023-05-01 12:22:00 -0700

    Initial Commit

---
+++ SingletonExperiment/SingletonExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SingletonExperimentApp.swift
+//  SingletonExperiment
+//
+//  Created by Aman Aggarwal on 5/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SingletonExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
